a6f2141d0850d5bbf554a0ae8994395381d2e7c3
kanirolf <wdomingo@uci.edu>
2015-11-22 08:32:59 -0800

    Refactored eInfo.

--- eInfo/eInfo.js
+++ eInfo/eInfo.js
@@ -1,41 +1,38 @@
 function openClipboard(){
-	document.getElementById('clipboard-open').style.display = 'block'; //open clipboard info popup
-	document.getElementById('idcard-open').style.display = 'none'; //close ID info popup
+	document.getElementById('clipboard').style.display = 'block'; //open clipboard info popup
+	document.getElementById('idcard').style.display = 'none'; //close ID info popup
 }
-
-function closeClipboard() {
-	document.getElementById('clipboard-open').style.display = 'none';
-}
-
 
 
 function openIDcard() {
-	document.getElementById('idcard-open').style.display = 'block'; //open ID info popup
-	document.getElementById('clipboard-open').style.display = 'none'; //close clipboard if open
+	document.getElementById('idcard').style.display = 'block'; //open ID info popup
+	document.getElementById('clipboard').style.display = 'none'; //close clipboard if open
 }
-
-function closeIDcard() {
-	document.getElementById('idcard-open').style.display = 'none';
-}
-
 
 // Print ID info:
 
-function printMembername() {
-	document.getElementById('idcard-membername').innerHTML = "<b>Name:</b><br>Emory Jiang";
+function printMembername(firstName, lastName) {
+	document.getElementById('idcard-membername').innerHTML = lastName + "," + firstName;
 }
 
-function printEmail() {
-	document.getElementById('idcard-email').innerHTML = "<b>Email:</b><br>emjiang@uci.edu";
+function printEmail(email) {
+	document.getElementById('idcard-email').innerHTML = email;
 }
 
-function printJoindate() {
-	document.getElementById('idcard-joindate').innerHTML = "<b>Join:</b><br>2015/11/21"
+function printJoindate(joinDate) {
+	document.getElementById('idcard-joindate').innerHTML = joinDate;
 	
 }
 
 window.onload = function (){
-	printMembername();
-	printEmail();
-	printJoindate();
+	var userData = {
+			firstName: "Emory",
+			lastName: "Jiang",
+			email: "emjiang@uci.edu",
+			joinDate: "2015/11/21"
+	};
+	
+	printMembername(userData.firstName, userData.lastName);
+	printEmail(userData.email);
+	printJoindate(userData.joinDate);
 }
